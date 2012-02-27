dfc3941b6b3f08bb10d8c41538a8bcaf6dda5141
Zviad Metreveli <zvio2002@gmail.com>
2012-02-27 14:01:45 -0800

    commit rpc handler

--- server/locator.py
+++ server/locator.py
@@ -1,13 +1,57 @@
+from __future__ import with_statement, absolute_import
+import functools
 import logging
 import os
+import simplejson
 import sys
+import threading
 
 import tornado
 import tornado.ioloop
 import tornado.web
 
-
 DEBUG_MODE = False
+
+# XXX: location samples device id-> list of scanResults
+samples = {}
+
+class RpcRequestHandler(tornado.web.RequestHandler):
+    RPC_METHODS = {
+        }
+
+    def __handle_request(self, args):
+        try:
+            args = simplejson.loads(args)
+            args = dict((str(k), v) for k, v in args.iteritems())
+
+            method = args.pop("method")
+        except Exception:
+            self.send_error(400)
+
+        if not method in RpcRequestHandler.RPC_METHODS:
+            self.send_error(404)
+
+        logging.info("RPC[%s]: args: %s" % (method, simplejson.dumps(args, sort_keys=True)))
+        self.__rpc_method = method
+        thread = threading.Thread(target = self.__async_rpc,
+                kwargs = {
+                    'method' : method,
+                    'kwargs' : args,
+                    })
+        thread.start()
+
+    def __async_rpc(self, method, kwargs):
+        ret = RpcRequestHandler.RPC_METHODS[method](**kwargs)
+        tornado.ioloop.IOLoop.instance().add_callback(functools.partial(self.__finish_request, ret))
+
+    def __finish_request(self, ret):
+        ret = simplejson.dumps(ret)
+        self.write(ret)
+        self.finish()
+
+    @tornado.web.asynchronous
+    def post(self):
+        return self.__handle_request(self.request.body)
 
 if __name__ == "__main__":
     assert __file__
