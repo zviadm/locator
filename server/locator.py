d18a3df080697c20103003f0ef7f22a89b933e21
Zviad Metreveli <zvio2002@gmail.com>
2012-02-27 17:50:12 -0800

    some janky android code for sample gathering

--- server/locator.py
+++ server/locator.py
@@ -10,6 +10,7 @@ import tornado
 import tornado.ioloop
 import tornado.web
 
+import rpcs
 DEBUG_MODE = False
 
 # XXX: location samples device id-> list of scanResults
@@ -17,6 +18,7 @@ samples = {}
 
 class RpcRequestHandler(tornado.web.RequestHandler):
     RPC_METHODS = {
+        "scan_result": rpcs.scan_result,
         }
 
     def __handle_request(self, args):
