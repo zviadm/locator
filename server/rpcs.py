8dfb0ce78a66b60234e7b1b6d27df64c5a888d27
Zviad Metreveli <zvio2002@gmail.com>
2012-02-27 22:16:07 -0800

    fix locdata indexing

--- server/rpcs.py
+++ server/rpcs.py
@@ -3,8 +3,20 @@ from __future__ import with_statement, absolute_import
 import os
 
 def location_sample(device_id, location_id, timestamp, scan_results):
-    for i, scan_result in enumerate(scan_results):
-        with open(os.path.join("/srv/locdata", "%s%02d.csv" % (location_id, i)), "a") as f:
-            f.write("%s,%s,%s\n" % (scan_result["SSID"], scan_result["BSSID"], scan_result["level"]))
+    LOCDATA_DIR = "/srv/locdata"
 
+    max_index = 0
+    csvfiles = os.listdir(LOCDATA_DIR)
+    for csvfile in csvfiles:
+        if csvfile.startswith(location_id):
+            try:
+                index = int(csvfile[location_id:-4])
+                if index > max_index:
+                    max_index = index
+            except ValueError:
+                pass
+
+    with open(os.path.join("/srv/locdata", "%s%d.csv" % (location_id, max_index + 1), "w")) as f:
+        for scan_result in scan_results:
+            f.write("%s,%s,%s\n" % (scan_result["SSID"], scan_result["BSSID"], scan_result["level"]))
 
